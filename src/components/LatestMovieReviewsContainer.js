024cc3275f9edeed6b0b973d8dcbb448bce69fde
Desiree Lerma <desi.lerma96@yahoo.com>
2021-03-04 02:02:05 -0600

    commit tree 64d0014ce6ba503d0843d97dad5ba51b278f45a6

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -7,3 +7,27 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+export default class LatestMovieReviewsContainer extends React.Component {
+
+    state = {
+        reviews: []
+    }
+
+    fetchLatestReviews = () => {
+        fetch(URL)
+        .then(resp => resp.json())
+        .then(jsonData => this.setState({reviews: jsonData.results}))
+    }
+
+    componentDidMount() {
+        this.fetchLatestReviews()
+    }
+
+    render() {
+        return(
+            <div className="latest-movie-reviews">
+                <MovieReviews reviews={this.state.reviews} />
+            </div>
+        )
+    }
+}
